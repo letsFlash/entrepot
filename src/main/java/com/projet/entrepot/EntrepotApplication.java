081c04ff49643afbc17cf81941eb070af90c9adf
mohamed <med.lhmaime@gmail.com>
2023-10-02 21:46:14 +0200

    first commit

---
+++ src/main/java/com/projet/entrepot/EntrepotApplication.java
@@ -0,0 +1,13 @@
+package com.projet.entrepot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EntrepotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EntrepotApplication.class, args);
+	}
+
+}
